e3af73ca08402c2e54d247c3f4549529288ce01f
jlevigt <jlevi.gt@gmail.com>
2024-05-12 21:37:58 -0300

    add database query

---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+export default {
+  query: query,
+};
