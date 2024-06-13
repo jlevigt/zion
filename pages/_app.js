8970de7326320001b5c069db3d4181197415cadc
jlevigt <jlevi.gt@gmail.com>
2024-06-13 19:04:19 -0300

    tentando corrigir bug em produção

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,6 @@ import "styles/forms.css";
 import "styles/lists.css";
 import "styles/tables.css";
 
-import Head from "next/head";
-
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <html lang="pt-BR" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
